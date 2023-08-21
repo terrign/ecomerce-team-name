20358240ff3b59ce940b82970979b95b32c52e24
Vladimir Kirpichyov <vlasoul@mail.ru>
2023-08-21 19:18:04 +0300

    feat: add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import './App.css';
 import MainRoutes from './routes/MainRoutes';
 import SideMenu from './components/SideMenu';
 import { HashRouter } from 'react-router-dom';
-import UserAvatar from './components/UserAvatar';
 import { useAppDispatch } from './store/hooks';
 import { actions as userMenuActions } from './store/userMenu.slice';
-const { Header, Content } = Layout;
+import Header from './components/Header';
+const { Content } = Layout;
 
 const App: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -18,9 +18,7 @@ const App: React.FC = () => {
       <Layout className="main-layout" style={{ minHeight: '100vh' }} onClick={onClick}>
         <SideMenu />
         <Layout className="site-layout">
-          <Header className="site-layout-background header">
-            <UserAvatar />
-          </Header>
+          <Header />
           <Content
             className="site-layout-background"
             style={{
