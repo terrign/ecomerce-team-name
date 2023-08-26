6c969118a787c56cd1334bd0a3855d65cb449ee2
terrign <vajipi@gmail.com>
2023-08-27 00:00:24 +0300

    feat:  redirect from user page fo unauthorized users

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAppSelector } from '../store/hooks';
+import { useNavigate } from 'react-router-dom';
+import { RouterPath } from '../models/RouterPath';
 const User = () => {
+  const isLogged = useAppSelector((state) => Boolean(state.auth.tokenStore.token));
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLogged) navigate(RouterPath.LOGIN);
+  }, []);
+
+  if (!isLogged) {
+    return <></>;
+  }
   return <div>User profile</div>;
 };
 
