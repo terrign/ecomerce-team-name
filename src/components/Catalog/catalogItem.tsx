8e383c9523bd2e494b1d81507298585470373625
anderskambenders <a.zetsmanis@mail.ru>
2023-08-28 23:10:20 +0300

    feat: add pagination

--- src/components/Catalog/catalogItem.tsx
+++ src/components/Catalog/catalogItem.tsx
@@ -1,33 +1,46 @@
 import React from 'react';
 import { Col } from 'antd';
 import { Card } from 'antd';
-import { EllipsisOutlined } from '@ant-design/icons';
 
 type CatalogItem = {
   name: string;
   description: string;
   image: string;
+  price: number;
+  discPrice: number;
 };
 
-const gridStyle: React.CSSProperties = {
-  width: '100%',
-  height: 150,
-  // textAlign: ',
-};
-const { Meta } = Card;
+// const gridStyleDesc: React.CSSProperties = {
+//   width: '100%',
+//   minHeight: 100,
+// };
+// const gridStylePrice: React.CSSProperties = {
+//   height: 20,
+// };
+
+// const { Meta } = Card;
 
 export const CatalogItem = (props: CatalogItem) => {
   return (
-    <Col flex={3}>
+    <Col flex={3} span={8}>
       <Card
+        bordered={false}
         hoverable
+        // loading={true}
         style={{ maxWidth: 240, height: '100%' }}
-        cover={<img alt="example" src={props.image} style={{ height: 200 }} />}
-        actions={[<EllipsisOutlined key="ellipsis" title="detail" label="more" />]}
+        cover={
+          <div style={{ height: 300, display: 'flex' }}>
+            <img alt="example" src={props.image} style={{ margin: 'auto', width: '100%', height: 'auto' }} />
+          </div>
+        }
       >
-        <Card.Grid style={gridStyle} hoverable={false}>
-          <Meta title={props.name} description={props.description} />
-        </Card.Grid>
+        {/* <Card.Grid style={gridStyleDesc} hoverable={false}>
+          <Meta title= description={props.description} />
+        </Card.Grid> */}
+        <p>{props.name}</p>
+        <p>{props.description}</p>
+        <p>{props.price / 100}$</p>
+        <p>{props.discPrice / 100}$</p>
       </Card>
     </Col>
   );
--- src/components/Catalog/catalogList.tsx
+++ src/components/Catalog/catalogList.tsx
@@ -1,32 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import getApiClient from '../../helpers/ApiClient/getApiClient';
-import { Row } from 'antd';
+import { Row, Pagination } from 'antd';
 import { CatalogItem } from './catalogItem';
 
 export const CatalogList = () => {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
   const getProducts = async () => {
     try {
-      const resp = await getApiClient().products().get().execute();
+      const resp = await getApiClient()
+        .products()
+        .get({ queryArgs: { offset: page } })
+        .execute();
       const data = resp.body.results;
-      console.log(data);
+      console.log(Math.ceil(resp.body.total / resp.body.limit));
+      // console.log(data);
+      // console.log(resp);
       setProducts(data);
+      setTotalPages(Math.floor(resp.body.total / resp.body.limit));
     } catch (err) {
       console.log(err);
     }
   };
   useEffect(() => {
     getProducts();
-  }, []);
+  }, [page]);
 
   return (
-    <Row gutter={[20, 30]}>
-      {products.map((prod, ind) => {
-        const name = prod?.masterData?.current?.name['en-US'];
-        const description = prod?.masterData?.current?.description['en-US'];
-        const image = prod?.masterData?.current?.masterVariant?.images[0].url;
-        return <CatalogItem key={ind} name={name} description={description} image={image} />;
-      })}
-    </Row>
+    <div>
+      <Row gutter={[20, 30]}>
+        {products.map((prod, ind) => {
+          const name = prod?.masterData?.current?.name?.en;
+          const description = prod?.masterData?.current?.description?.en;
+          const image = prod?.masterData?.current?.masterVariant?.images[0].url;
+          const price = prod?.masterData?.current?.masterVariant?.prices[0]?.value?.centAmount;
+          const discPrice = prod?.masterData?.current?.masterVariant?.prices[0]?.discounted?.value?.centAmount;
+          return (
+            <CatalogItem
+              key={ind}
+              name={name}
+              description={description}
+              image={image}
+              price={price}
+              discPrice={discPrice}
+            />
+          );
+        })}
+      </Row>
+      <Pagination
+        defaultCurrent={1}
+        total={totalPages * 10}
+        onChange={(page) => {
+          setPage(page * 20);
+        }}
+      />
+    </div>
   );
 };
