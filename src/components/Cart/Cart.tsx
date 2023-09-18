e1ec80002d12b28d9a39e7c75825b3aeb90a3230
anderskambenders <a.zetsmanis@mail.ru>
2023-09-18 23:43:58 +0300

    feat: add selector and fix discount

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,4 +1,4 @@
-import { Button, List, Popconfirm, Typography, Input, Form } from 'antd';
+import { Button, List, Popconfirm, Typography, Select, Form } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import clearCart from '../../helpers/ApiClient/cart/clearCart';
@@ -9,6 +9,7 @@ import './Cart.css';
 import { store } from '../../store/store';
 import { cartSlice } from '../../store/cart.slice';
 import { alertSlice } from '../../store/alert.slice';
+import { removeCartDiscount } from '../../helpers/ApiClient/cart/removeCartDiscount';
 
 const Cart = () => {
   const dispatch = useAppDispatch();
@@ -16,7 +17,7 @@ const Cart = () => {
   const items = lineItemsReducer(cart?.lineItems);
   const [form] = Form.useForm();
   const [priceNoDiscount, setPriceNoDiscount] = useState(null);
-  const isDiscounted = cart.discountCodes.length > 0;
+  const [isDiscounted, setIsDiscounted] = useState(cart.discountCodes.length > 0);
 
   const onClear = () => {
     clearCart();
@@ -32,7 +33,12 @@ const Cart = () => {
       }, 0) / 100
     );
   };
-  console.log(priceNoDiscount);
+
+  const removeDiscount = async () => {
+    removeCartDiscount();
+    setIsDiscounted(false);
+  };
+
   const onFinish = async () => {
     const code = form.getFieldsValue().promocode;
     if (cart.discountCodes.length === 0) {
@@ -54,7 +60,7 @@ const Cart = () => {
         .execute();
       setPriceNoDiscount(countPriceNoDiscount());
       store.dispatch(cartSlice.actions.set(resp.body));
-      form.resetFields();
+      setIsDiscounted(true);
     } else {
       dispatch(alertSlice.actions.error('Sorry, promocode already used'));
     }
@@ -71,7 +77,30 @@ const Cart = () => {
       <div className="cart-wrapper">
         <Form style={{ marginBottom: 20 }} layout="inline" onFinish={onFinish} form={form}>
           <Form.Item style={{ marginBottom: 5 }} name={'promocode'}>
-            <Input placeholder="Enter your promocode" />
+            <Select
+              allowClear
+              onClear={removeDiscount}
+              placeholder="Your promo here"
+              optionFilterProp="children"
+              options={[
+                {
+                  value: 'PROMO5',
+                  label: 'PROMO5',
+                },
+                {
+                  value: 'PROMO10',
+                  label: 'PROMO10',
+                },
+                {
+                  value: 'PROMO15',
+                  label: 'PROMO15',
+                },
+                {
+                  value: 'PROMO20',
+                  label: 'PROMO20',
+                },
+              ]}
+            />
           </Form.Item>
           <Form.Item>
             <Button type="primary" htmlType="submit">
@@ -79,7 +108,6 @@ const Cart = () => {
             </Button>
           </Form.Item>
         </Form>
-
         <List
           dataSource={items}
           renderItem={(itemData) => <CartItem {...itemData}></CartItem>}
@@ -98,7 +126,7 @@ const Cart = () => {
                   </Button>
                 </Popconfirm>
                 <Typography.Title style={{ fontSize: '1.5rem', textAlign: 'end' }}>
-                  {priceNoDiscount && <p style={{ textDecoration: 'line-through' }}>{priceNoDiscount}$</p>}
+                  {isDiscounted && <p style={{ textDecoration: 'line-through' }}>{priceNoDiscount}$</p>}
                   Total: {cart.totalPrice.centAmount / 100}$
                 </Typography.Title>
               </div>
